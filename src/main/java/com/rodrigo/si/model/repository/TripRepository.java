16c9af30cc8b8eaf8037429c8b3d4284e99de9c6
rodrigot23 <rodrigoguedest23@gmail.com>
2021-04-03 03:13:09 -0400

    Added functions to search trips connections

--- src/main/java/com/rodrigo/si/model/repository/TripRepository.java
+++ src/main/java/com/rodrigo/si/model/repository/TripRepository.java
@@ -1,9 +1,28 @@
 package com.rodrigo.si.model.repository;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.rodrigo.si.model.Trip;
 
 public interface TripRepository extends JpaRepository<Trip, String>{
 
+	@Query("select t from trip t where (t.origin.station = :ori or t.destiny.station = :dest) "
+			+ "and (:depart is null or t.departureDate = :depart)")
+	Optional<List<Trip>> findByOriginAndDestinyAndDeparture(
+			@Param("ori") String origin, 
+			@Param("dest") String destiny, 
+			@Param("depart") LocalDate departure);
+	
+	@Query("select t from trip t where t.departure > :depart and t.arrival < :arrival ")
+	Optional<List<Trip>> findByDepartureGreateThanAndArrivalLessThan(
+			@Param("depart") LocalTime departure, 
+			@Param("arrival") LocalTime arrival);
+	
 }
