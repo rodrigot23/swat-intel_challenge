7fe073bc501b165364a42de50a64b9a287edb30e
rodrigot23 <rodrigoguedest23@gmail.com>
2021-04-05 01:54:57 -0400

    Added the responsabilities of removed class ConnectionTripResource

--- src/main/java/com/rodrigo/si/resource/TripResource.java
+++ src/main/java/com/rodrigo/si/resource/TripResource.java
@@ -1,8 +1,9 @@
 package com.rodrigo.si.resource;
 
-import java.util.List;
+import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.rodrigo.si.model.Trip;
 import com.rodrigo.si.resource.projection.TripDTO;
+import com.rodrigo.si.resource.projection.TripQuery;
+import com.rodrigo.si.resource.projection.TripRouteDTO;
 import com.rodrigo.si.service.TripService;
+import com.rodrigo.si.service.connection.ConnectionService;
 
 @RestController
 @RequestMapping("/trip")
@@ -25,31 +28,39 @@ public class TripResource {
 	@Autowired
 	private TripService tripServ;
 	
+	@Autowired
+	private ConnectionService connectionService;
+	
 	@PostMapping
-	@ResponseStatus(HttpStatus.OK)
+	@ResponseStatus(HttpStatus.CREATED)
 	public void saveTrip(@RequestBody TripDTO trip) throws Exception {
 		tripServ.save(trip);
 	}
 	
 	@PostMapping("/csv")
-	@ResponseStatus(HttpStatus.OK)
+	@ResponseStatus(HttpStatus.CREATED)
 	public void saveTripCsv(@RequestParam("file") MultipartFile file, @RequestParam String company) throws Exception {
 		tripServ.batchCsv(file, company);
 	}
 	
 	@PostMapping("/json")
-	@ResponseStatus(HttpStatus.OK)
+	@ResponseStatus(HttpStatus.CREATED)
 	public void saveTripJson(@RequestParam("file") MultipartFile file, @RequestParam String company) throws Exception {
 		tripServ.batchJson(file, company);
 	}
 	
-	@GetMapping
-	public ResponseEntity<List<Trip>> listTrips() throws Exception {
-		var result = tripServ.listAll();
-		if (result.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	@GetMapping("/connection")
+	public ResponseEntity<TripRouteDTO> getConnectionTrip(@RequestParam String origin, @RequestParam String destiny, @RequestParam(required=false) @DateTimeFormat(pattern="dd/MM/yyyy") LocalDate date) {
+		
+		var trip = new TripQuery();
+		trip.setOrigin(origin);
+		trip.setDestiny(destiny);
+		trip.setDate(date);
+		
+		var tripRoute = connectionService.getConnectionTrip(trip);
+		if (tripRoute.isPresent()) {
+			return ResponseEntity.ok(tripRoute.get());
 		}
-		return ResponseEntity.ok(result.get());
+		return ResponseEntity.noContent().build();
 	}
-	
 }
