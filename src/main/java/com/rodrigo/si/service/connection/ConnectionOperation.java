ee15aded7ec3a9ffe6cac6f5531a430fd006ded0
rodrigot23 <rodrigoguedest23@gmail.com>
2021-04-05 01:49:37 -0400

    Refactored rules of filtering data, in special, added a departureDate rule

--- src/main/java/com/rodrigo/si/service/connection/ConnectionOperation.java
+++ src/main/java/com/rodrigo/si/service/connection/ConnectionOperation.java
@@ -15,26 +15,38 @@ public class ConnectionOperation {
 
 	private BiPredicate<Trip, String> originFilter = null;
 	private BiPredicate<Trip, String> destinyFilter = null;
-	private BiPredicate<Trip, Trip> hourFilter = null;
+	private BiPredicate<Trip, Trip> departureDateAndHourFilter = null;
 	private BiPredicate<Trip, Trip> maximumHour = null;
 	private Comparator<Trip> arrivalOrder = null;
+	private Comparator<Trip> departureDate = null;
 
 	public ConnectionOperation() {
 		buildRules();
 	}
 
+	public BiPredicate<Trip, Trip> getDepartureDateAndHourFilter() {
+		return departureDateAndHourFilter;
+	}
+	
 	private void buildRules() {
 		this.originFilter = (e, o) -> e.getOrigin().getStation().equals(o);
 		this.destinyFilter = (e, o) -> e.getDestiny().getStation().equals(o);
-		this.hourFilter = (e, o) -> e.getDeparture().isAfter(o.getArrival());
+		this.departureDateAndHourFilter = (e, o) -> {
+			return (Objects.isNull(o)) ? true : (e.getDepartureDate().isAfter(o.getDepartureDate()) 
+					|| e.getDepartureDate().isEqual(o.getDepartureDate()))
+					&& e.getDeparture().isAfter(o.getArrival());
+		};
 		this.maximumHour = (e, o) -> e.getDeparture().getHour() - o.getArrival().getHour() <= 12;
 		this.arrivalOrder = (e1, e2) ->  e1.getArrival().compareTo(e2.getArrival());
+		this.departureDate = (e1, e2) ->  e1.getDepartureDate().compareTo(e2.getDepartureDate());
 	}
 
-	public List<Trip> getSingleResult(List<Trip> trips, String origin, String destiny) {
+	public List<Trip> getSingleResult(List<Trip> trips, String origin, String destiny, Trip previousTrip) {
 		var single = trips.stream()
 				.filter(e -> originFilter.test(e, origin))
 				.filter(e -> destinyFilter.test(e, destiny))
+				.filter(e -> departureDateAndHourFilter.test(e, previousTrip))
+				.sorted(departureDate)
 				.sorted(arrivalOrder)
 				.collect(Collectors.toList());
 		return single;
@@ -43,13 +55,16 @@ public class ConnectionOperation {
 	public List<Trip> getAllByOrigin(List<Trip> trips, String origin) {
 		return trips.stream()
 				.filter(e -> originFilter.test(e, origin))
+				.sorted(departureDate)
 				.sorted(arrivalOrder)
 				.collect(Collectors.toList());
 	}
 
-	public List<Trip> getAllByDestiny(List<Trip> trips, String destiny) {
+	public List<Trip> getAllByDestiny(List<Trip> trips, String destiny, Trip previousTrip) {
 		return trips.stream()
 				.filter(e -> destinyFilter.test(e, destiny))
+				.filter(e -> departureDateAndHourFilter.test(e, previousTrip))
+				.sorted(departureDate)
 				.sorted(arrivalOrder)
 				.collect(Collectors.toList());
 	}
@@ -61,8 +76,9 @@ public class ConnectionOperation {
 	public List<Trip> getPreviousStep(List<Trip> trips, Trip nextStep) {
 		return trips.stream()
 				.filter(e -> destinyFilter.test(e, nextStep.getOrigin().getStation()))
-				.filter(e -> hourFilter.test(nextStep, e))
+				.filter(e -> departureDateAndHourFilter.test(nextStep, e))
 				.filter(e -> maximumHour.test(nextStep, e))
+				.sorted(departureDate)
 				.sorted(arrivalOrder)
 				.collect(Collectors.toList());
 	}
@@ -72,12 +88,13 @@ public class ConnectionOperation {
 			.flatMap(o -> {
 				var destinations = trips.stream()
 						.filter(e -> originFilter.test(e, o.getDestiny().getStation()))
-						.filter(e -> hourFilter.test(e, o))
+						.filter(e -> departureDateAndHourFilter.test(e, o))
 						.filter(e -> maximumHour.test(e, o))
 						.collect(Collectors.toList());
 				return destinations.isEmpty() ? null : destinations.stream();
 				})
 			.filter(Objects::nonNull)
+			.sorted(departureDate)
 			.sorted(arrivalOrder)
 			.collect(Collectors.toList());
 	}
