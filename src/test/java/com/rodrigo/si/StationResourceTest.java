2e335b44a8a822c794c9d81c42d8aba5561e2b8d
rodrigot23 <rodrigoguedest23@gmail.com>
2021-04-05 01:56:45 -0400

    A new test integration for StationResource

---
+++ src/test/java/com/rodrigo/si/StationResourceTest.java
@@ -0,0 +1,74 @@
+package com.rodrigo.si;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.HashMap;
+
+import org.json.JSONObject;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(OrderAnnotation.class)
+public class StationResourceTest {
+
+	@Autowired
+	public MockMvc mvc;
+
+	@Test
+	@Order(1)
+	public void uploadStationsTest() throws Exception {
+		var fileBytes = getClass()
+				.getResourceAsStream("/resources/backend-aptitude-challenge-main/trainStations.json")
+				.readAllBytes();
+		
+		mvc.perform(multipart("/station/json").file("file", fileBytes))
+		   .andExpect(status().isCreated());
+	}
+
+	@Test
+	@Order(2)
+	public void saveStationTest() throws Exception {
+		
+		var body = new HashMap<String, String>();
+		body.put("name", "Estação Juscelino Kubitschek");
+		body.put("station", "BSB");
+		body.put("city", "Brasília");
+		
+		var json = new JSONObject(body);
+		
+		mvc.perform(
+				post("/station")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json.toString()))
+			.andExpect(status().isCreated());
+	}
+	
+	@Test
+	@Order(3)
+	public void saveStationsAndGetStationsByNameTest() throws Exception {
+		
+		mvc.perform(get("/station/juscelino"))
+		   .andExpect(status().isOk())
+		   .andExpect(jsonPath("$.[0].name", is("Estação Juscelino Kubitschek")))
+		   .andExpect(jsonPath("$.[0].station", is("BSB")))
+		   .andExpect(jsonPath("$.[0].city", is("Brasília")));
+
+	}
+}
