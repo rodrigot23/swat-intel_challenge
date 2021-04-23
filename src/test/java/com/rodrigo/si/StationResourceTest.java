c0230b8eb3c0761ccd3eeae14f1a9794e5cc1148
rodrigot23 <rodrigoguedest23@gmail.com>
2021-04-23 00:23:31 -0400

    removing SpringRunner as a unit runner

--- src/test/java/com/rodrigo/si/StationResourceTest.java
+++ src/test/java/com/rodrigo/si/StationResourceTest.java
@@ -14,15 +14,12 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestMethodOrder(OrderAnnotation.class)
